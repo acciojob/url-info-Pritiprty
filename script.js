fdbee700c3b688cc009d89a780d0504d1bcfb2ef
Priti Rai <138751304+Pritiprty@users.noreply.github.com>
2024-08-06 06:02:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL of the page
+const currentUrl = window.location.href;
+
+// Count the number of letters in the URL
+const urlLength = currentUrl.length;
+
+// Print the final length of the URL
+alert(`The length of the URL is: ${urlLength}`);
